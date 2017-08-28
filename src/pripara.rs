d3afbcdfe785d6ad6cc4d431156f0e13a69078b0
Kohei Suzuki <eagletmt@gmail.com>
2017-08-29 01:06:58 +0900

    Run pripara scraper concurrently

--- src/pripara.rs
+++ src/pripara.rs
@@ -10,48 +10,53 @@ pub fn update_all() {
 
     let mut core = tokio_core::reactor::Core::new().expect("Failed to initialize tokio reactor");
     let client = hyper::Client::new(&core.handle());
+    let mut works = vec![];
     for pref in fetch_prefs(&mut core, &client) {
         use self::futures::Future;
-
-        let mut csv_writer = super::CsvWriter::new(format!("pripara/{}.csv", pref))
-            .expect("Failed to open CSV file");
-        csv_writer.write_header().expect(
-            "Failed to write CSV header",
-        );
 
         let uri = format!("http://pripara.jp/shop/search_list?pref_name={}", pref)
             .parse()
             .expect("Failed to parse search_list URL");
         info!("GET {}", uri);
-        let work = client.get(uri).and_then(|res| {
-            use self::futures::Stream;
-            use self::kuchiki::traits::TendrilSink;
-            use std::borrow::Borrow;
+        let work = client
+            .get(uri)
+            .and_then(|res| {
+                use self::futures::Stream;
+                use self::kuchiki::traits::TendrilSink;
+                use std::borrow::Borrow;
 
-            info!("{}", res.status());
-            res.body().concat2().and_then(|body| {
-                let document = kuchiki::parse_html().one(String::from_utf8_lossy(&body).borrow());
+                info!("{}", res.status());
+                res.body().concat2().and_then(|body| {
+                    let document =
+                        kuchiki::parse_html().one(String::from_utf8_lossy(&body).borrow());
 
-                let mut shops = vec![];
-                for shop_node in document.select("div.h2Tbl").unwrap() {
-                    if let Some(shop) = extract_shop(shop_node) {
-                        shops.push(shop);
+                    let mut shops = vec![];
+                    for shop_node in document.select("div.h2Tbl").unwrap() {
+                        if let Some(shop) = extract_shop(shop_node) {
+                            shops.push(shop);
+                        }
                     }
-                }
-                Ok(shops)
+                    Ok(shops)
+                })
             })
-        });
-        let shops = core.run(work).expect("Failed to run tokio event loop");
-        if shops.is_empty() {
-            break;
-        }
-
-        for shop in shops {
-            csv_writer.write_shop(&shop).expect(
-                "Unable to write shops to file",
-            );
-        }
+            .and_then(move |shops| {
+                let mut csv_writer = super::CsvWriter::new(format!("pripara/{}.csv", pref))
+                    .expect("Failed to open CSV file");
+                csv_writer.write_header().expect(
+                    "Failed to write CSV header",
+                );
+                for shop in shops {
+                    csv_writer.write_shop(&shop).expect(
+                        "Unable to write shops to file",
+                    );
+                }
+                Ok(())
+            });
+        works.push(work);
     }
+    core.run(futures::future::join_all(works)).expect(
+        "Failed to run tokio event loop",
+    );
 }
 
 fn fetch_prefs(
