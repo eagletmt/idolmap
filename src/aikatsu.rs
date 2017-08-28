7894f56f15660d8ad03366a7675b9aea973464e6
Kohei Suzuki <eagletmt@gmail.com>
2017-08-29 02:58:16 +0900

    Get pages in each pref concurrently

--- src/aikatsu.rs
+++ src/aikatsu.rs
@@ -4,6 +4,7 @@ extern crate kuchiki;
 extern crate selectors;
 extern crate std;
 extern crate tokio_core;
+extern crate url;
 
 pub fn update_all() {
     std::fs::create_dir_all("aikatsu").expect("Failed to create aikatsu directory");
@@ -13,23 +14,17 @@ pub fn update_all() {
     let prefs = fetch_prefs(&mut core, &client);
 
     for (pref_id, pref) in prefs {
-        let mut csv_writer = super::CsvWriter::new(format!("aikatsu/{:02}.csv", pref))
-            .expect("Failed to open CSV file");
-        csv_writer.write_header().expect(
-            "Failed to write CSV header",
+        use self::futures::Future;
+
+        let base_uri_str = format!(
+            "http://www.aikatsu.com/stars/playshop/list.php?p=1&pref={:02}",
+            pref_id
         );
-
-        for page in 1.. {
-            use self::futures::Future;
-
-            let uri = format!(
-                "http://www.aikatsu.com/stars/playshop/list.php?p={}&pref={}",
-                page,
-                pref_id
-            ).parse()
-                .expect("Failed to parse playshop URL");
-            info!("GET {}", uri);
-            let work = client.get(uri).and_then(|res| {
+        let base_uri = base_uri_str.parse().unwrap();
+        let base_url = url::Url::parse(&base_uri_str).unwrap();
+        let work = client
+            .get(base_uri)
+            .and_then(|res| {
                 use self::futures::Stream;
                 use self::kuchiki::traits::TendrilSink;
                 use std::borrow::Borrow;
@@ -38,21 +33,70 @@ pub fn update_all() {
                 res.body().concat2().and_then(|body| {
                     let document =
                         kuchiki::parse_html().one(String::from_utf8_lossy(&body).borrow());
-
-                    let mut shops = vec![];
-                    for shop_node in document.select("table.shoplist_resultlist").unwrap() {
-                        if let Some(shop) = extract_shop(shop_node) {
-                            shops.push(shop);
-                        }
+                    let last_page_node = document
+                        .select("div.paginator > a")
+                        .unwrap()
+                        .last()
+                        .expect("No a element in paginator");
+                    let last_page_element = last_page_node.as_node().as_element().unwrap();
+                    let attributes = last_page_element.attributes.borrow();
+                    if let Some(href) = attributes.get("href") {
+                        let last_page_url =
+                            base_url.join(href).expect("Failed to parse last_page URL");
+                        let pair = last_page_url.query_pairs().find(|p| p.0 == "p").expect(
+                            "Failed to find p parameter",
+                        );
+                        Ok(pair.1.parse().expect("Non-integer p parameter"))
+                    } else {
+                        // No pagination
+                        Ok(1)
                     }
-                    Ok(shops)
                 })
-            });
-            let shops = core.run(work).expect("Failed to run tokio event loop");
-            if shops.is_empty() {
-                break;
-            }
+            })
+            .and_then(|last_page| {
+                let mut works = vec![];
+                for page in 0..last_page {
+                    use self::futures::Future;
 
+                    let uri = format!(
+                        "http://www.aikatsu.com/stars/playshop/list.php?p={}&pref={:02}",
+                        page + 1,
+                        pref_id
+                    ).parse()
+                        .expect("Failed to parse playshop URL");
+                    info!("GET {}", uri);
+                    let work = client.get(uri).and_then(|res| {
+                        use self::futures::Stream;
+                        use self::kuchiki::traits::TendrilSink;
+                        use std::borrow::Borrow;
+
+                        info!("{}", res.status());
+                        res.body().concat2().and_then(|body| {
+                            let document =
+                                kuchiki::parse_html().one(String::from_utf8_lossy(&body).borrow());
+
+                            let mut shops = vec![];
+                            for shop_node in document.select("table.shoplist_resultlist").unwrap() {
+                                if let Some(shop) = extract_shop(shop_node) {
+                                    shops.push(shop);
+                                }
+                            }
+                            Ok(shops)
+                        })
+                    });
+                    works.push(work)
+                }
+                futures::future::join_all(works)
+            });
+
+        let shops_per_page = core.run(work).expect("Failed to run tokio event loop");
+        let mut csv_writer = super::CsvWriter::new(format!("aikatsu/{:02}.csv", pref))
+            .expect("Failed to open CSV file");
+        csv_writer.write_header().expect(
+            "Failed to write CSV header",
+        );
+
+        for shops in shops_per_page {
             for shop in shops {
                 csv_writer.write_shop(&shop).expect(
                     "Unable to write shops to file",
